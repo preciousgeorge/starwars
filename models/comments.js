ff42a34f66919081b06bf3bf15f1fcf9a16dde7a
Precious George <precious.george.o@gmail.com>
2019-05-09 11:11:18 +0100

    -- removed association script from comments model

--- models/comments.js
+++ models/comments.js
@@ -1,12 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Comments = sequelize.define('Comments', {
-    filmId: DataTypes.INTEGER,
-    comment: DataTypes.TEXT,
-    userIp: DataTypes.STRING
-  }, {});
-  Comments.associate = function(models) {
-    // associations can be defined here
-  };
+  const Comments = sequelize.define(
+    'Comments',
+    {
+      filmId: DataTypes.INTEGER,
+      comment: DataTypes.TEXT,
+      userIp: DataTypes.STRING
+    },
+    {}
+  );
+
   return Comments;
 };
