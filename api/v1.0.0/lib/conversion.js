a55937140aaeaf6967ae7eed1a1a3b4fb5087f15
nativity <precious.george.o@gmail.com>
2019-07-30 00:16:29 +0100

    Update conversion.js

--- api/v1.0.0/lib/conversion.js
+++ api/v1.0.0/lib/conversion.js
@@ -1,7 +1,7 @@
 function toFeet(cm) {
-  var realFeet = (cm * 0.3937) / 12;
-  var feet = Math.floor(realFeet);
-  var inches = Math.ceil((realFeet - feet) * 12 * 100) / 100;
+  let realFeet = (cm * 0.3937) / 12;
+  let feet = Math.floor(realFeet);
+  let inches = Math.ceil((realFeet - feet) * 12 * 100) / 100;
   return feet + 'ft and ' + inches + 'inches';
 }
 
