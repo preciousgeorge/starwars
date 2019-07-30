7ff90186270978c2930661a291fab7ed01d709ae
nativity <precious.george.o@gmail.com>
2019-07-30 09:34:44 +0100

    Update conversion.js

--- api/v1.0.0/lib/conversion.js
+++ api/v1.0.0/lib/conversion.js
@@ -1,4 +1,4 @@
-function toFeet(cm) {
+const toFeet = cm => {
   let realFeet = (cm * 0.3937) / 12;
   let feet = Math.floor(realFeet);
   let inches = Math.ceil((realFeet - feet) * 12 * 100) / 100;
